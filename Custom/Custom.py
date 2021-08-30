4d2b7865b5cdd462094de3ec0bc2793f0b9acf73
Kastytis Zubovas <laurie.lassi@gmail.com>
2021-08-30 11:43:53 +0300

    added plotRel for plotting relative differences of profiles

--- Custom/Custom.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-print("sekmingai paloadintas Custom.py")
-print("nezymiai pakeistas")
-def Hi(vardas):
-    print("Hi hello "+vardas)
-
-def Bye(vardas):
-    print("Bye "+vardas)
